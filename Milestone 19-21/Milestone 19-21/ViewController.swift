af3187b412570b621fd507161fbd7a966b45a60c
Artem <artchzhen@gmail.com>
2023-05-10 18:35:40 +0900

    added tilte, navigationController, change view for table controller, added func numberofRows, RoadCell

--- Milestone 19-21/Milestone 19-21/ViewController.swift
+++ Milestone 19-21/Milestone 19-21/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    lazy var notes = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Notes"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewController", for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row]
+        return cell
+    }
 }
 
